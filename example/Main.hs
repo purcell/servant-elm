7d9c27b25da8862f96183c8644eeacdc36cacb0f
Steve Purcell <steve@sanityinc.com>
2015-12-18 08:41:45 +1300

    Generate code using info from servant-foreign

--- example/Main.hs
+++ example/Main.hs
@@ -1,18 +1,25 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE TypeFamilies  #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
 
 module Main where
 
-import           Control.Monad.Trans.Either
+import           Control.Monad.Trans.Except
 import           Data.Aeson
+import           Data.Text
+import           Data.Text.IO                as TIO
 import           GHC.Generics
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Network.Wai.Middleware.Cors
 import           Servant
 import           Servant.Elm
+import           System.Environment          (getArgs)
 
 type UserAPI = "users" :> Get '[JSON] [User]
           :<|> "discovery" :> Capture "id" Int :> Get '[JSON] Discovery
@@ -30,12 +37,12 @@ data Discovery = Discovery
 instance ToJSON User
 instance ToJSON Discovery
 
-users :: EitherT ServantErr IO [User]
+users :: ExceptT ServantErr IO [User]
 users = return [ User "Isaac Newton"    372
   , User "Albert Einstein" 136
   ]
 
-discovery :: Int -> EitherT ServantErr IO Discovery
+discovery :: Int -> ExceptT ServantErr IO Discovery
 discovery date = return (Discovery "The thing" date)
 
 server :: Server UserAPI
@@ -49,5 +56,8 @@ app = simpleCors $ serve userAPI server
 
 main :: IO ()
 main = do
-  putStrLn $ elmForAPI userAPI
+  outfile:_ <- getArgs
+  Prelude.putStrLn $ "Writing elm to " ++ outfile
+  TIO.writeFile outfile (elmForAPI userAPI)
+  Prelude.putStrLn "Listening on http://localhost:8081/"
   run 8081 app
