f4bff958afaef7385483fba87a3f3254b66768b3
Andy Newport <andy.newport@powershop.co.nz>
2015-12-17 13:55:07 +1300

    Add trivial example

--- example/Main.hs
+++ example/Main.hs
@@ -1,6 +1,47 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
-import           Servant.Elm
+import Data.Aeson
+import Data.Time.Calendar
+import GHC.Generics
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.Elm
+
+import qualified Data.Text    as T
+import qualified Data.Text.IO as T
+
+type UserAPI = "users" :> Get '[JSON] [User]
+
+data User = User
+  { name :: String
+  , age :: Int
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON User
+
+users :: [User]
+users =
+  [ User "Isaac Newton"    372
+  , User "Albert Einstein" 136
+  ]
+
+server :: Server UserAPI
+server = return users
+
+userAPI :: Proxy UserAPI
+userAPI = Proxy
+
+app :: Application
+app = serve userAPI server
 
 main :: IO ()
-main = return ()
+main = do
+  putStrLn "Some string"
+  putStrLn $ elmForAPI userAPI
+  run 8081 app
