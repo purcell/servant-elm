948751b8a1e9dfa68cb69753250a90d79cf1688c
Andy Newport <andy.newport@powershop.co.nz>
2015-12-17 16:39:43 +1300

    Add different endpoint

--- example/Main.hs
+++ example/Main.hs
@@ -10,6 +10,7 @@ import Data.Time.Calendar
 import GHC.Generics
 import Network.Wai
 import Network.Wai.Handler.Warp
+import Control.Monad.Trans.Either
 import Servant
 import Servant.Elm
 
@@ -17,22 +18,31 @@ import qualified Data.Text    as T
 import qualified Data.Text.IO as T
 
 type UserAPI = "users" :> Get '[JSON] [User]
+          :<|> "discovery" :> Capture "id" Int :> Get '[JSON] Discovery
 
 data User = User
   { name :: String
   , age :: Int
   } deriving (Eq, Show, Generic)
 
-instance ToJSON User
+data Discovery = Discovery
+  { details :: String
+  , year :: Int
+  } deriving (Eq, Show, Generic)
 
-users :: [User]
-users =
-  [ User "Isaac Newton"    372
+instance ToJSON User
+instance ToJSON Discovery
+
+users :: EitherT ServantErr IO [User]
+users = return [ User "Isaac Newton"    372
   , User "Albert Einstein" 136
   ]
 
+discovery :: Int -> EitherT ServantErr IO Discovery
+discovery date = return (Discovery "The thing" date)
+
 server :: Server UserAPI
-server = return users
+server = users :<|> discovery
 
 userAPI :: Proxy UserAPI
 userAPI = Proxy
@@ -42,6 +52,5 @@ app = serve userAPI server
 
 main :: IO ()
 main = do
-  putStrLn "Some string"
   putStrLn $ elmForAPI userAPI
   run 8081 app
